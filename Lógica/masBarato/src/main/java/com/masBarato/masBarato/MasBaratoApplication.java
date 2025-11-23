b4a074f1aaa747a7073b6aa716a77e5add4b7faa
Anthony <anthonyperdomogonzalez9@gmail>
2025-11-22 22:24:40 -0500

    commit de la lógica

---
+++ Lógica/masBarato/src/main/java/com/masBarato/masBarato/MasBaratoApplication.java
@@ -0,0 +1,13 @@
+package com.masBarato.masBarato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasBaratoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasBaratoApplication.class, args);
+	}
+
+}
---
+++ Lógica/masBarato/src/test/java/com/masBarato/masBarato/MasBaratoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masBarato.masBarato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasBaratoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
